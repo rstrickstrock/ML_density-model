17fe8866e39c885f4fd6b4c18c762fc5ab47369a
Robo <rstrickstrock@gmail.com>
2025-01-24 14:24:27 +0100

    polyRegr plots

--- 02.2_Polynomial_regression/03_plotRatioVsRMSE_R2.py
+++ 02.2_Polynomial_regression/03_plotRatioVsRMSE_R2.py
@@ -33,155 +33,147 @@ maxR2 = dfStatistics['r2'].max()
 maxR2 = maxR2 + 0.01*maxR2
 
 subsetGrid1296 = dfStatistics[dfStatistics["dataset"] == "Grid1296"]
-print(f'Grid1296')
-print(f'mean RMSE: {np.mean(subsetGrid1296["rmse"].to_numpy())}')
-print(f'stddev RMSE: {np.std(subsetGrid1296["rmse"].to_numpy())}')
-print(f'mean R2: {np.mean(subsetGrid1296["r2"].to_numpy())}')
-print(f'stddev R2: {np.std(subsetGrid1296["r2"].to_numpy())}\n')
+#print(f'Grid1296')
+#print(f'mean RMSE: {np.mean(subsetGrid1296["rmse"].to_numpy())}')
+#print(f'stddev RMSE: {np.std(subsetGrid1296["rmse"].to_numpy())}')
+#print(f'mean R2: {np.mean(subsetGrid1296["r2"].to_numpy())}')
+#print(f'stddev R2: {np.std(subsetGrid1296["r2"].to_numpy())}\n')
+#print(f'mean RMSE: {np.mean(subsetGrid1296["rmse"].to_numpy())}')
 subsetGrid2401 = dfStatistics[dfStatistics["dataset"] == "Grid2401"]
-print(f'Grid2401')
-print(f'mean RMSE: {np.mean(subsetGrid2401["rmse"].to_numpy())}')
-print(f'stddev RMSE: {np.std(subsetGrid2401["rmse"].to_numpy())}')
-print(f'mean R2: {np.mean(subsetGrid2401["r2"].to_numpy())}')
-print(f'stddev R2: {np.std(subsetGrid2401["r2"].to_numpy())}\n')
+#print(f'Grid2401')
+#print(f'mean RMSE: {np.mean(subsetGrid2401["rmse"].to_numpy())}')
+#print(f'stddev RMSE: {np.std(subsetGrid2401["rmse"].to_numpy())}')
+#print(f'mean R2: {np.mean(subsetGrid2401["r2"].to_numpy())}')
+#print(f'stddev R2: {np.std(subsetGrid2401["r2"].to_numpy())}\n')
 subsetSobol1 = dfStatistics[dfStatistics["dataset"] == "Sobol1"]
-print(f'Sobol1')
-print(f'mean RMSE: {np.mean(subsetSobol1["rmse"].to_numpy())}')
-print(f'stddev RMSE: {np.std(subsetSobol1["rmse"].to_numpy())}')
-print(f'mean R2: {np.mean(subsetSobol1["r2"].to_numpy())}')
-print(f'stddev R2: {np.std(subsetSobol1["r2"].to_numpy())}\n')
+#print(f'Sobol1')
+#print(f'mean RMSE: {np.mean(subsetSobol1["rmse"].to_numpy())}')
+#print(f'stddev RMSE: {np.std(subsetSobol1["rmse"].to_numpy())}')
+#print(f'mean R2: {np.mean(subsetSobol1["r2"].to_numpy())}')
+#print(f'stddev R2: {np.std(subsetSobol1["r2"].to_numpy())}\n')
 subsetSobol2 = dfStatistics[dfStatistics["dataset"] == "Sobol2"]
-print(f'Sobol2')
-print(f'mean RMSE: {np.mean(subsetSobol2["rmse"].to_numpy())}')
-print(f'stddev RMSE: {np.std(subsetSobol2["rmse"].to_numpy())}')
-print(f'mean R2: {np.mean(subsetSobol2["r2"].to_numpy())}')
-print(f'stddev R2: {np.std(subsetSobol2["r2"].to_numpy())}\n')
+#print(f'Sobol2')
+#print(f'mean RMSE: {np.mean(subsetSobol2["rmse"].to_numpy())}')
+#print(f'stddev RMSE: {np.std(subsetSobol2["rmse"].to_numpy())}')
+#print(f'mean R2: {np.mean(subsetSobol2["r2"].to_numpy())}')
+#print(f'stddev R2: {np.std(subsetSobol2["r2"].to_numpy())}\n')
+if False:
+  print(f'Grid1296')
+  for d in range(1, 11):  # Loop through degrees 1 to 10
+    print(
+        f'& {d} & '
+        f'{np.round(np.mean(subsetGrid1296[subsetGrid1296["degree"] == d]["rmse"].to_numpy()), 4)} & '
+        f'{np.round(np.std(subsetGrid1296[subsetGrid1296["degree"] == d]["rmse"].to_numpy()), 4)} & '
+        f'{np.round(np.mean(subsetGrid1296[subsetGrid1296["degree"] == d]["r2"].to_numpy()), 4)} & '
+        f'{np.round(np.std(subsetGrid1296[subsetGrid1296["degree"] == d]["r2"].to_numpy()), 4)} \\\\'
+    )
+  print('')
+  print(f'Grid2401')
+  for d in range(1, 11):  # Loop through degrees 1 to 10
+    print(
+        f'& {d} & '
+        f'{np.round(np.mean(subsetGrid2401[subsetGrid2401["degree"] == d]["rmse"].to_numpy()), 4)} & '
+        f'{np.round(np.std(subsetGrid2401[subsetGrid2401["degree"] == d]["rmse"].to_numpy()), 4)} & '
+        f'{np.round(np.mean(subsetGrid2401[subsetGrid2401["degree"] == d]["r2"].to_numpy()), 4)} & '
+        f'{np.round(np.std(subsetGrid2401[subsetGrid2401["degree"] == d]["r2"].to_numpy()), 4)} \\\\'
+    )
+  print('')
+  print(f'Sobol1')
+  for d in range(1, 11):  # Loop through degrees 1 to 10
+    print(
+        f'& {d} & '
+        f'{np.round(np.mean(subsetSobol1[subsetSobol1["degree"] == d]["rmse"].to_numpy()), 4)} & '
+        f'{np.round(np.std(subsetSobol1[subsetSobol1["degree"] == d]["rmse"].to_numpy()), 4)} & '
+        f'{np.round(np.mean(subsetSobol1[subsetSobol1["degree"] == d]["r2"].to_numpy()), 4)} & '
+        f'{np.round(np.std(subsetSobol1[subsetSobol1["degree"] == d]["r2"].to_numpy()), 4)} \\\\'
+    )
+  print('')
+  print(f'Sobol2')
+  for d in range(1, 11):  # Loop through degrees 1 to 10
+    print(
+        f'& {d} & '
+        f'{np.round(np.mean(subsetSobol2[subsetSobol2["degree"] == d]["rmse"].to_numpy()), 4)} & '
+        f'{np.round(np.std(subsetSobol2[subsetSobol2["degree"] == d]["rmse"].to_numpy()), 4)} & '
+        f'{np.round(np.mean(subsetSobol2[subsetSobol2["degree"] == d]["r2"].to_numpy()), 4)} & '
+        f'{np.round(np.std(subsetSobol2[subsetSobol2["degree"] == d]["r2"].to_numpy()), 4)} \\\\'
+    )
+  print('')
+
 
 ## plots
-gs_kw = dict(width_ratios=[1, 1], height_ratios=[1, 1])
-fig, axd = plt.subplot_mosaic([['Grid1296', 'Sobol1'], 
-                               ['Grid2401', 'Sobol2']], 
-                               gridspec_kw=gs_kw, figsize=(14.0, 14.0))
+degs = [5, 5, 6, 6]
+datasetSubsets = [subsetGrid1296, subsetGrid2401, subsetSobol1, subsetSobol2]
+#print(f'{datasetSubsets[0]}')
+colors = ['#377eb8', '#ff7f00', '#4daf4a', '#f781bf', '#a65628', '#984ea3', '#999999', '#e41a1c', '#dede00']
+markers = ['1', '2', '3', '4', 'x', '+']
+DatasetNames = ['Grid1296', 'Grid2401', 'Sobol1', 'Sobol2']
+for deg in range(0, len(degs)):
+  #print(f'deg: {deg}')
+  thisDataSubset = datasetSubsets[deg]
+  gs_kw = dict(width_ratios=[1, 1, 1], height_ratios=[1])
+  fig, axd = plt.subplot_mosaic([['RMSE', 'R2', 'AvgByDegree']],  
+                                 gridspec_kw=gs_kw, figsize=(15.0, 5.0))
 
-axd2Grid1296 = axd["Grid1296"].twinx()
-axd2Grid2401 = axd["Grid2401"].twinx()
-axd2Sobol1 = axd["Sobol1"].twinx()
-axd2Sobol2 = axd["Sobol2"].twinx()
-
-labeled = False
-for ratio in subsetGrid1296["ratio"].unique():
-  thisSubset = subsetGrid1296[subsetGrid1296["ratio"] == ratio]
-  thisAvgRMSE = np.mean(thisSubset["rmse"].to_numpy())
-  thisStdRMSE = np.std(thisSubset["rmse"].to_numpy())
-  thisAvgR2 = np.mean(thisSubset["r2"].to_numpy())
-  thisStdR2 = np.std(thisSubset["r2"].to_numpy())
-  if not labeled:
-    axd["Grid1296"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, label="RMSE", c="#069AF3", marker='o')
-    axd2Grid1296.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, label="R2", c="#F97306", marker='o')
-  else:
-    axd["Grid1296"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, c="#069AF3", marker='o')
-    axd2Grid1296.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, c="#F97306", marker='o')
+  DegreesForPlotting = []
+  AvgRMSE = []
+  AvgRMSEerr = []
+  AvgR2 = []
+  AvgR2err = []
+  for d in range(1, degs[deg]+1):
+    thisSubsetDegree = thisDataSubset[thisDataSubset["degree"] == d]
+    #print(f'{thisSubsetDegree}')
+    DegreesForPlotting.append(d)
+    AvgRMSE.append(np.mean(thisSubsetDegree["rmse"].to_numpy()))
+    AvgRMSEerr.append(np.std(thisSubsetDegree["rmse"].to_numpy()))
+    AvgR2.append(np.mean(thisSubsetDegree["r2"].to_numpy()))
+    AvgR2err.append(np.std(thisSubsetDegree["r2"].to_numpy()))
+    thisX = []
+    thisRMSE = []
+    thisRMSEerr = []
+    thisR2 = []
+    thisR2err = []
+    for ratio in thisSubsetDegree["ratio"].unique():
+      thisX.append(1-ratio)
+      thisSubsetDegreeRatio = thisSubsetDegree[thisSubsetDegree["ratio"] == ratio]
+      thisRMSE.append(np.mean(thisSubsetDegreeRatio["rmse"].to_numpy()))
+      thisRMSEerr.append(np.std(thisSubsetDegreeRatio["rmse"].to_numpy()))
+      thisR2.append(np.mean(thisSubsetDegreeRatio["r2"].to_numpy()))
+      thisR2err.append(np.std(thisSubsetDegreeRatio["r2"].to_numpy()))
+    axd['RMSE'].errorbar(thisX, thisRMSE, yerr=thisRMSEerr, label=f'polyn. degree = {d}', marker=markers[d-1], color=colors[d-1], ls='-.')
+    axd['R2'].errorbar(thisX, thisR2, yerr=thisR2err, label=f'polyn. degree = {d}', marker=markers[d-1], color=colors[d-1], ls='-.')
     
-  thisSubset = subsetGrid2401[subsetGrid2401["ratio"] == ratio]
-  thisAvgRMSE = np.mean(thisSubset["rmse"].to_numpy())
-  thisStdRMSE = np.std(thisSubset["rmse"].to_numpy())
-  thisAvgR2 = np.mean(thisSubset["r2"].to_numpy())
-  thisStdR2 = np.std(thisSubset["r2"].to_numpy())
-  if not labeled:
-    axd["Grid2401"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, label="RMSE", c="#069AF3", marker='o')
-    axd2Grid2401.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, label="R2", c="#F97306", marker='o')
-  else:
-    axd["Grid2401"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, c="#069AF3", marker='o')
-    axd2Grid2401.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, c="#F97306", marker='o')
-    
-  thisSubset = subsetSobol1[subsetSobol1["ratio"] == ratio]
-  thisAvgRMSE = np.mean(thisSubset["rmse"].to_numpy())
-  thisStdRMSE = np.std(thisSubset["rmse"].to_numpy())
-  thisAvgR2 = np.mean(thisSubset["r2"].to_numpy())
-  thisStdR2 = np.std(thisSubset["r2"].to_numpy())
-  if not labeled:
-    axd["Sobol1"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, label="RMSE", c="#069AF3", marker='o')
-    axd2Sobol1.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, label="R2", c="#F97306", marker='o')
-  else:
-    axd["Sobol1"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, c="#069AF3", marker='o')
-    axd2Sobol1.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, c="#F97306", marker='o')
-    
-  thisSubset = subsetSobol2[subsetSobol2["ratio"] == ratio]
-  thisAvgRMSE = np.mean(thisSubset["rmse"].to_numpy())
-  thisStdRMSE = np.std(thisSubset["rmse"].to_numpy())
-  thisAvgR2 = np.mean(thisSubset["r2"].to_numpy())
-  thisStdR2 = np.std(thisSubset["r2"].to_numpy())
-  if not labeled:
-    axd["Sobol2"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, label="RMSE", c="#069AF3", marker='o')
-    axd2Sobol2.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, label="R2", c="#F97306", marker='o')
-  else:
-    axd["Sobol2"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, c="#069AF3", marker='o')
-    axd2Sobol2.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, c="#F97306", marker='o')
-    
-  labeled = True
+  axd["RMSE"].legend()
+  axd["RMSE"].set_ylabel("RMSE", fontweight='bold')
+  axd["RMSE"].set_xlabel("% of Dataset used for Training", fontweight='bold')
+  axd["RMSE"].set_title("Avg. RMSE per Training-/Testdata split", fontweight='bold')
+  #axd["RMSE"].set_ylim([minRMSE, maxRMSE])
+  axd["R2"].legend()
+  axd["R2"].set_xlabel("% of Dataset used for Training", fontweight='bold')
+  axd["R2"].set_ylabel("R2", fontweight='bold')
+  axd["R2"].set_title("Avg. R2 per Training-/Testdata split", fontweight='bold')
+  axd["R2"].set_ylim([-1.0, 1.0])
   
-axd["Grid1296"].legend(bbox_to_anchor=(0.99,0.99))
-axd["Grid1296"].set_xlabel("% of Dataset used for Training")
-axd["Grid1296"].set_ylabel("RMSE", c="#069AF3")
-axd["Grid1296"].set_title("Dataset used for Training: Grid1296", fontweight='bold')
-#axd["Grid1296"].set_ylim([minRMSE, maxRMSE])
-axd["Grid1296"].tick_params(axis='y', color="#069AF3", which='both')
-axd["Grid1296"].spines['left'].set_color("#069AF3")
-axd2Grid1296.legend(bbox_to_anchor=(0.99,0.94))
-axd2Grid1296.set_ylabel("R2", c="#F97306")
-#axd2Grid1296.set_ylim([minR2, maxR2])
-axd2Grid1296.spines['left'].set_color("#069AF3")
-axd2Grid1296.spines['right'].set_color("#F97306")
-axd2Grid1296.tick_params(axis='y', color="#F97306", which='both')
-
-
-axd["Grid2401"].legend(bbox_to_anchor=(0.18,0.99))
-axd["Grid2401"].set_xlabel("% of Dataset used for Training")
-axd["Grid2401"].set_ylabel("RMSE", c="#069AF3")
-axd["Grid2401"].set_title("Dataset used for Training: Grid2401", fontweight='bold')
-#axd["Grid2401"].set_ylim([minRMSE, maxRMSE])
-axd["Grid2401"].tick_params(axis='y', color="#069AF3", which='both')
-axd["Grid2401"].spines['left'].set_color("#069AF3")
-axd2Grid2401.legend(bbox_to_anchor=(0.18,0.94))
-axd2Grid2401.set_ylabel("R2", c="#F97306")
-#axd2Grid2401.set_ylim([minR2, maxR2])
-axd2Grid2401.spines['left'].set_color("#069AF3")
-axd2Grid2401.spines['right'].set_color("#F97306")
-axd2Grid2401.tick_params(axis='y', color="#F97306", which='both')
-
-axd["Sobol1"].legend(bbox_to_anchor=(0.99,0.99))
-axd["Sobol1"].set_xlabel("% of Dataset used for Training")
-axd["Sobol1"].set_ylabel("RMSE", c="#069AF3")
-axd["Sobol1"].set_title("Dataset used for Training: Sobol1", fontweight='bold')
-#axd["Sobol1"].set_ylim([minRMSE, maxRMSE])
-axd["Sobol1"].tick_params(axis='y', color="#069AF3", which='both')
-axd["Sobol1"].spines['left'].set_color("#069AF3")
-axd2Sobol1.legend(bbox_to_anchor=(0.99,0.94))
-axd2Sobol1.set_ylabel("R2", c="#F97306")
-#axd2Sobol1.set_ylim([minR2, maxR2])
-axd2Sobol1.spines['left'].set_color("#069AF3")
-axd2Sobol1.spines['right'].set_color("#F97306")
-axd2Sobol1.tick_params(axis='y', color="#F97306", which='both')
-
-axd["Sobol2"].legend(bbox_to_anchor=(0.99,0.99))
-axd["Sobol2"].set_xlabel("% of Dataset used for Training")
-axd["Sobol2"].set_ylabel("RMSE", c="#069AF3")
-axd["Sobol2"].set_title("Dataset used for Training: Sobol2", fontweight='bold')
-#axd["Sobol2"].set_ylim([minRMSE, maxRMSE])
-axd["Sobol2"].tick_params(axis='y', color="#069AF3", which='both')
-axd["Sobol2"].spines['left'].set_color("#069AF3")
-axd2Sobol2.legend(bbox_to_anchor=(0.99,0.94))
-axd2Sobol2.set_ylabel("R2", c="#F97306")
-#axd2Sobol2.set_ylim([minR2, maxR2])
-axd2Sobol2.spines['left'].set_color("#069AF3")
-axd2Sobol2.spines['right'].set_color("#F97306")
-axd2Sobol2.tick_params(axis='y', color="#F97306", which='both')
-
-plt.tight_layout()
+  axd['AvgByDegree'].errorbar(DegreesForPlotting, AvgRMSE, yerr=AvgRMSEerr, label=f'Average RMSEs', marker='o', color='#069AF3', ls='-.')
+  axd['AvgByDegree'].legend(bbox_to_anchor=(0.22,0.05), loc='center')
+  axd["AvgByDegree"].set_ylabel("RMSE", c="#069AF3", fontweight='bold')
+  axd["AvgByDegree"].set_xlabel("Polynomial Degree", fontweight='bold')
+  axd["AvgByDegree"].set_title("Avg. RMSE/R2 per degree", fontweight='bold')
+  axd["AvgByDegree"].set_xticks(DegreesForPlotting)
+  axd["AvgByDegree"].tick_params(axis='y', color="#069AF3", which='both')
+  axd["AvgByDegree"].spines['left'].set_color("#069AF3")
+  axd2 = axd["AvgByDegree"].twinx()
+  axd2.errorbar(DegreesForPlotting, AvgR2, yerr=AvgR2err, label=f'Average R2s', marker='o', color='#F97306', ls='-.')
+  axd2.legend(bbox_to_anchor=(0.8,0.05), loc='center')
+  axd2.set_ylabel("R2", c="#F97306", fontweight='bold')
+  axd2.set_ylim([-1.0, 1.0])
+  axd2.spines['left'].set_color("#069AF3")
+  axd2.spines['right'].set_color("#F97306")
+  axd2.tick_params(axis='y', color="#F97306", which='both')
+  
+  fig.suptitle(f'Dataset used for Training: {DatasetNames[deg]}', fontweight='bold')
+  plt.tight_layout()
+  #break
 plt.show()
-
-
-
 
 
 
