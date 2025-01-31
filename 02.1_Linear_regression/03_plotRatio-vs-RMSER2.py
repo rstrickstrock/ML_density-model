6dd62ba5ba6dd3a88952d0207c67b08b2c9e98c4
Admin Istrator <rstrickstrock@gmail.com>
2025-01-31 10:19:09 +0100

    linRegr 03_plots..

--- 02.1_Linear_regression/03_plotRatio-vs-RMSER2.py
+++ 02.1_Linear_regression/03_plotRatio-vs-RMSER2.py
@@ -5,8 +5,31 @@ import numpy as np
 import os
 import glob
 
-
+metricX = "rmse"
+#metricX = "mape"
+metricY = "r2"
 statisticsFile = 'Stats.csv'
+
+if not metricX in ["rmse", "mape", "r2"]:
+  print(f'Chosen metric 1 ({metricX}) is not in [rmse, mape, r2]. Please chose one of them. Exit.')
+  exit()
+if not metricY in ["rmse", "mape", "r2"]:
+  print(f'Chosen metric 2 ({metricY}) is not in [rmse, mape, r2]. Please chose one of them. Exit.')
+  exit()
+  
+if metricX is "rmse":
+  xLabel = "RMSE"
+elif metricX is "mape":
+  xLabel = "MAPE"
+else:
+  xLabel = "R2"
+
+if metricY is "rmse":
+  yLabel = "RMSE"
+elif metricY is "mape":
+  yLabel = "MAPE"
+else:
+  yLabel = "R2"
 
 if not os.path.isfile(statisticsFile):
   print(f'Can not find and open \'{statisticsFile}\'. Exit.')
@@ -23,39 +46,39 @@ else:
     pass
 
 
-minRMSE = dfStatistics['rmse'].min()
-minRMSE = minRMSE - 0.01*minRMSE
-maxRMSE = dfStatistics['rmse'].max()
-maxRMSE = maxRMSE + 0.01*maxRMSE
-minR2 = dfStatistics['r2'].min()
-minR2 = minR2 - 0.001*minR2
-maxR2 = dfStatistics['r2'].max()
-maxR2 = maxR2 + 0.01*maxR2
+minMetric1 = dfStatistics[f'{metricX}'].min()
+minMetric1 = minMetric1 - 0.01*minMetric1
+maxMetric1 = dfStatistics[f'{metricX}'].max()
+maxMetric1 = maxMetric1 + 0.01*maxMetric1
+minMetric2 = dfStatistics['r2'].min()
+minMetric2 = minMetric2 - 0.001*minMetric2
+maxMetric2 = dfStatistics['r2'].max()
+maxMetric2 = maxMetric2 + 0.01*maxMetric2
 
 subsetGrid1296 = dfStatistics[dfStatistics["dataset"] == "Grid1296"]
 print(f'Grid1296')
-print(f'mean RMSE: {np.mean(subsetGrid1296["rmse"].to_numpy())}')
-print(f'stddev RMSE: {np.std(subsetGrid1296["rmse"].to_numpy())}')
-print(f'mean R2: {np.mean(subsetGrid1296["r2"].to_numpy())}')
-print(f'stddev R2: {np.std(subsetGrid1296["r2"].to_numpy())}\n')
+print(f'mean RMSE: {np.mean(subsetGrid1296[f'{metricX}'].to_numpy())}')
+print(f'stddev RMSE: {np.std(subsetGrid1296[f'{metricX}'].to_numpy())}')
+print(f'mean R2: {np.mean(subsetGrid1296[f'{metricY}'].to_numpy())}')
+print(f'stddev R2: {np.std(subsetGrid1296[f'{metricY}'].to_numpy())}\n')
 subsetGrid2401 = dfStatistics[dfStatistics["dataset"] == "Grid2401"]
 print(f'Grid2401')
-print(f'mean RMSE: {np.mean(subsetGrid2401["rmse"].to_numpy())}')
-print(f'stddev RMSE: {np.std(subsetGrid2401["rmse"].to_numpy())}')
-print(f'mean R2: {np.mean(subsetGrid2401["r2"].to_numpy())}')
-print(f'stddev R2: {np.std(subsetGrid2401["r2"].to_numpy())}\n')
+print(f'mean RMSE: {np.mean(subsetGrid2401[f'{metricX}'].to_numpy())}')
+print(f'stddev RMSE: {np.std(subsetGrid2401[f'{metricX}'].to_numpy())}')
+print(f'mean R2: {np.mean(subsetGrid2401[f'{metricY}'].to_numpy())}')
+print(f'stddev R2: {np.std(subsetGrid2401[f'{metricY}'].to_numpy())}\n')
 subsetSobol1 = dfStatistics[dfStatistics["dataset"] == "Sobol1"]
 print(f'Sobol1')
-print(f'mean RMSE: {np.mean(subsetSobol1["rmse"].to_numpy())}')
-print(f'stddev RMSE: {np.std(subsetSobol1["rmse"].to_numpy())}')
-print(f'mean R2: {np.mean(subsetSobol1["r2"].to_numpy())}')
-print(f'stddev R2: {np.std(subsetSobol1["r2"].to_numpy())}\n')
+print(f'mean RMSE: {np.mean(subsetSobol1[f'{metricX}'].to_numpy())}')
+print(f'stddev RMSE: {np.std(subsetSobol1[f'{metricX}'].to_numpy())}')
+print(f'mean R2: {np.mean(subsetSobol1[f'{metricY}'].to_numpy())}')
+print(f'stddev R2: {np.std(subsetSobol1[f'{metricY}'].to_numpy())}\n')
 subsetSobol2 = dfStatistics[dfStatistics["dataset"] == "Sobol2"]
 print(f'Sobol2')
-print(f'mean RMSE: {np.mean(subsetSobol2["rmse"].to_numpy())}')
-print(f'stddev RMSE: {np.std(subsetSobol2["rmse"].to_numpy())}')
-print(f'mean R2: {np.mean(subsetSobol2["r2"].to_numpy())}')
-print(f'stddev R2: {np.std(subsetSobol2["r2"].to_numpy())}\n')
+print(f'mean RMSE: {np.mean(subsetSobol2[f'{metricX}'].to_numpy())}')
+print(f'stddev RMSE: {np.std(subsetSobol2[f'{metricX}'].to_numpy())}')
+print(f'mean R2: {np.mean(subsetSobol2[f'{metricY}'].to_numpy())}')
+print(f'stddev R2: {np.std(subsetSobol2[f'{metricY}'].to_numpy())}\n')
 
 ## plots
 gs_kw = dict(width_ratios=[1, 1], height_ratios=[1, 1])
@@ -71,49 +94,49 @@ axd2Sobol2 = axd["Sobol2"].twinx()
 labeled = False
 for ratio in subsetGrid1296["ratio"].unique():
   thisSubset = subsetGrid1296[subsetGrid1296["ratio"] == ratio]
-  thisAvgRMSE = np.mean(thisSubset["rmse"].to_numpy())
-  thisStdRMSE = np.std(thisSubset["rmse"].to_numpy())
-  thisAvgR2 = np.mean(thisSubset["r2"].to_numpy())
-  thisStdR2 = np.std(thisSubset["r2"].to_numpy())
+  thisAvgRMSE = np.mean(thisSubset[f'{metricX}'].to_numpy())
+  thisStdRMSE = np.std(thisSubset[f'{metricX}'].to_numpy())
+  thisAvgR2 = np.mean(thisSubset[f'{metricY}'].to_numpy())
+  thisStdR2 = np.std(thisSubset[f'{metricY}'].to_numpy())
   if not labeled:
-    axd["Grid1296"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, label="RMSE", c="#069AF3", marker='o')
-    axd2Grid1296.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, label="R2", c="#F97306", marker='o')
+    axd["Grid1296"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, label=f'{xLabel}', c="#069AF3", marker='o')
+    axd2Grid1296.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, label=f'{yLabel}', c="#F97306", marker='o')
   else:
     axd["Grid1296"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, c="#069AF3", marker='o')
     axd2Grid1296.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, c="#F97306", marker='o')
     
   thisSubset = subsetGrid2401[subsetGrid2401["ratio"] == ratio]
-  thisAvgRMSE = np.mean(thisSubset["rmse"].to_numpy())
-  thisStdRMSE = np.std(thisSubset["rmse"].to_numpy())
-  thisAvgR2 = np.mean(thisSubset["r2"].to_numpy())
-  thisStdR2 = np.std(thisSubset["r2"].to_numpy())
+  thisAvgRMSE = np.mean(thisSubset[f'{metricX}'].to_numpy())
+  thisStdRMSE = np.std(thisSubset[f'{metricX}'].to_numpy())
+  thisAvgR2 = np.mean(thisSubset[f'{metricY}'].to_numpy())
+  thisStdR2 = np.std(thisSubset[f'{metricY}'].to_numpy())
   if not labeled:
-    axd["Grid2401"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, label="RMSE", c="#069AF3", marker='o')
-    axd2Grid2401.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, label="R2", c="#F97306", marker='o')
+    axd["Grid2401"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, label=f'{xLabel}', c="#069AF3", marker='o')
+    axd2Grid2401.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, label=f'{yLabel}', c="#F97306", marker='o')
   else:
     axd["Grid2401"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, c="#069AF3", marker='o')
     axd2Grid2401.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, c="#F97306", marker='o')
     
   thisSubset = subsetSobol1[subsetSobol1["ratio"] == ratio]
-  thisAvgRMSE = np.mean(thisSubset["rmse"].to_numpy())
-  thisStdRMSE = np.std(thisSubset["rmse"].to_numpy())
-  thisAvgR2 = np.mean(thisSubset["r2"].to_numpy())
-  thisStdR2 = np.std(thisSubset["r2"].to_numpy())
+  thisAvgRMSE = np.mean(thisSubset[f'{metricX}'].to_numpy())
+  thisStdRMSE = np.std(thisSubset[f'{metricX}'].to_numpy())
+  thisAvgR2 = np.mean(thisSubset[f'{metricY}'].to_numpy())
+  thisStdR2 = np.std(thisSubset[f'{metricY}'].to_numpy())
   if not labeled:
-    axd["Sobol1"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, label="RMSE", c="#069AF3", marker='o')
-    axd2Sobol1.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, label="R2", c="#F97306", marker='o')
+    axd["Sobol1"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, label=f'{xLabel}', c="#069AF3", marker='o')
+    axd2Sobol1.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, label=f'{yLabel}', c="#F97306", marker='o')
   else:
     axd["Sobol1"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, c="#069AF3", marker='o')
     axd2Sobol1.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, c="#F97306", marker='o')
     
   thisSubset = subsetSobol2[subsetSobol2["ratio"] == ratio]
-  thisAvgRMSE = np.mean(thisSubset["rmse"].to_numpy())
-  thisStdRMSE = np.std(thisSubset["rmse"].to_numpy())
-  thisAvgR2 = np.mean(thisSubset["r2"].to_numpy())
-  thisStdR2 = np.std(thisSubset["r2"].to_numpy())
+  thisAvgRMSE = np.mean(thisSubset[f'{metricX}'].to_numpy())
+  thisStdRMSE = np.std(thisSubset[f'{metricX}'].to_numpy())
+  thisAvgR2 = np.mean(thisSubset[f'{metricY}'].to_numpy())
+  thisStdR2 = np.std(thisSubset[f'{metricY}'].to_numpy())
   if not labeled:
-    axd["Sobol2"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, label="RMSE", c="#069AF3", marker='o')
-    axd2Sobol2.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, label="R2", c="#F97306", marker='o')
+    axd["Sobol2"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, label=f'{xLabel}', c="#069AF3", marker='o')
+    axd2Sobol2.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, label=f'{yLabel}', c="#F97306", marker='o')
   else:
     axd["Sobol2"].errorbar(1-ratio, thisAvgRMSE, yerr=thisStdRMSE, c="#069AF3", marker='o')
     axd2Sobol2.errorbar(1-ratio, thisAvgR2, yerr=thisStdR2, c="#F97306", marker='o')
@@ -122,14 +145,14 @@ for ratio in subsetGrid1296["ratio"].unique():
   
 axd["Grid1296"].legend(bbox_to_anchor=(0.99,0.99))
 axd["Grid1296"].set_xlabel("% of Dataset used for Training")
-axd["Grid1296"].set_ylabel("RMSE", c="#069AF3")
+axd["Grid1296"].set_ylabel(f'{xLabel}', c="#069AF3")
 axd["Grid1296"].set_title("Dataset used for Training: Grid1296", fontweight='bold')
-axd["Grid1296"].set_ylim([minRMSE, maxRMSE])
+axd["Grid1296"].set_ylim([minMetric1, maxMetric1])
 axd["Grid1296"].tick_params(axis='y', color="#069AF3", which='both')
 axd["Grid1296"].spines['left'].set_color("#069AF3")
 axd2Grid1296.legend(bbox_to_anchor=(0.99,0.94))
-axd2Grid1296.set_ylabel("R2", c="#F97306")
-axd2Grid1296.set_ylim([minR2, maxR2])
+axd2Grid1296.set_ylabel(f'{yLabel}', c="#F97306")
+axd2Grid1296.set_ylim([minMetric2, maxMetric2])
 axd2Grid1296.spines['left'].set_color("#069AF3")
 axd2Grid1296.spines['right'].set_color("#F97306")
 axd2Grid1296.tick_params(axis='y', color="#F97306", which='both')
@@ -137,42 +160,42 @@ axd2Grid1296.tick_params(axis='y', color="#F97306", which='both')
 
 axd["Grid2401"].legend(bbox_to_anchor=(0.18,0.99))
 axd["Grid2401"].set_xlabel("% of Dataset used for Training")
-axd["Grid2401"].set_ylabel("RMSE", c="#069AF3")
+axd["Grid2401"].set_ylabel(f'{xLabel}', c="#069AF3")
 axd["Grid2401"].set_title("Dataset used for Training: Grid2401", fontweight='bold')
-axd["Grid2401"].set_ylim([minRMSE, maxRMSE])
+axd["Grid2401"].set_ylim([minMetric1, maxMetric1])
 axd["Grid2401"].tick_params(axis='y', color="#069AF3", which='both')
 axd["Grid2401"].spines['left'].set_color("#069AF3")
 axd2Grid2401.legend(bbox_to_anchor=(0.18,0.94))
-axd2Grid2401.set_ylabel("R2", c="#F97306")
-axd2Grid2401.set_ylim([minR2, maxR2])
+axd2Grid2401.set_ylabel(f'{yLabel}', c="#F97306")
+axd2Grid2401.set_ylim([minMetric2, maxMetric2])
 axd2Grid2401.spines['left'].set_color("#069AF3")
 axd2Grid2401.spines['right'].set_color("#F97306")
 axd2Grid2401.tick_params(axis='y', color="#F97306", which='both')
 
 axd["Sobol1"].legend(bbox_to_anchor=(0.99,0.99))
 axd["Sobol1"].set_xlabel("% of Dataset used for Training")
-axd["Sobol1"].set_ylabel("RMSE", c="#069AF3")
+axd["Sobol1"].set_ylabel(f'{xLabel}', c="#069AF3")
 axd["Sobol1"].set_title("Dataset used for Training: Sobol1", fontweight='bold')
-axd["Sobol1"].set_ylim([minRMSE, maxRMSE])
+axd["Sobol1"].set_ylim([minMetric1, maxMetric1])
 axd["Sobol1"].tick_params(axis='y', color="#069AF3", which='both')
 axd["Sobol1"].spines['left'].set_color("#069AF3")
 axd2Sobol1.legend(bbox_to_anchor=(0.99,0.94))
-axd2Sobol1.set_ylabel("R2", c="#F97306")
-axd2Sobol1.set_ylim([minR2, maxR2])
+axd2Sobol1.set_ylabel(f'{yLabel}', c="#F97306")
+axd2Sobol1.set_ylim([minMetric2, maxMetric2])
 axd2Sobol1.spines['left'].set_color("#069AF3")
 axd2Sobol1.spines['right'].set_color("#F97306")
 axd2Sobol1.tick_params(axis='y', color="#F97306", which='both')
 
 axd["Sobol2"].legend(bbox_to_anchor=(0.99,0.99))
 axd["Sobol2"].set_xlabel("% of Dataset used for Training")
-axd["Sobol2"].set_ylabel("RMSE", c="#069AF3")
+axd["Sobol2"].set_ylabel(f'{xLabel}', c="#069AF3")
 axd["Sobol2"].set_title("Dataset used for Training: Sobol2", fontweight='bold')
-axd["Sobol2"].set_ylim([minRMSE, maxRMSE])
+axd["Sobol2"].set_ylim([minMetric1, maxMetric1])
 axd["Sobol2"].tick_params(axis='y', color="#069AF3", which='both')
 axd["Sobol2"].spines['left'].set_color("#069AF3")
 axd2Sobol2.legend(bbox_to_anchor=(0.99,0.94))
-axd2Sobol2.set_ylabel("R2", c="#F97306")
-axd2Sobol2.set_ylim([minR2, maxR2])
+axd2Sobol2.set_ylabel(f'{yLabel}', c="#F97306")
+axd2Sobol2.set_ylim([minMetric2, maxMetric2])
 axd2Sobol2.spines['left'].set_color("#069AF3")
 axd2Sobol2.spines['right'].set_color("#F97306")
 axd2Sobol2.tick_params(axis='y', color="#F97306", which='both')
