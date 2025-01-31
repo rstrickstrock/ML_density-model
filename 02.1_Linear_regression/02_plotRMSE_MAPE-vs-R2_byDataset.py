9a9937680a815f3e4fc592f4aa36e47bf725c6e9
Admin Istrator <rstrickstrock@gmail.com>
2025-01-31 09:15:41 +0100

    min/max fix

--- 02.1_Linear_regression/02_plotRMSE_MAPE-vs-R2_byDataset.py
+++ 02.1_Linear_regression/02_plotRMSE_MAPE-vs-R2_byDataset.py
@@ -34,8 +34,8 @@ else:
 
 minMETRIC = dfStatistics[f'{metrictype}'].min()
 minMETRIC = minMETRIC - 0.01*minMETRIC
-minMETRIC = dfStatistics[f'{metrictype}'].max()
-minMETRIC = minMETRIC + 0.01*minMETRIC
+maxMETRIC = dfStatistics[f'{metrictype}'].max()
+maxMETRIC = maxMETRIC + 0.01*maxMETRIC
 minR2 = dfStatistics['r2'].min()
 minR2 = minR2 - 0.001*minR2
 maxR2 = dfStatistics['r2'].max()
@@ -59,35 +59,35 @@ axd["AllInOne"].scatter(subsetSobol2[f'{metrictype}'], subsetSobol2["r2"], label
 axd["AllInOne"].legend()
 axd["AllInOne"].set(xlabel=f'{xLabel}', ylabel="R2")
 axd["AllInOne"].set_title("All In One", fontweight='bold')
-axd["AllInOne"].set_xlim([minMETRIC, minMETRIC])
+axd["AllInOne"].set_xlim([minMETRIC, maxMETRIC])
 axd["AllInOne"].set_ylim([minR2, maxR2])
 
 axd["Grid1296"].scatter(subsetGrid1296[f'{metrictype}'], subsetGrid1296["r2"], label="Grid1296", c="#332288", marker='o')#, edgecolor="#44AA99")
 axd["Grid1296"].legend()
 axd["Grid1296"].set(xlabel=f'{xLabel}', ylabel="R2")
 axd["Grid1296"].set_title("Grid1296", fontweight='bold')
-axd["Grid1296"].set_xlim([minMETRIC, minMETRIC])
+axd["Grid1296"].set_xlim([minMETRIC, maxMETRIC])
 axd["Grid1296"].set_ylim([minR2, maxR2])
 
 axd["Grid2401"].scatter(subsetGrid2401[f'{metrictype}'], subsetGrid2401["r2"], label="Grid2401", c="#88CCEE", marker='o')#, edgecolor="#44AA99")
 axd["Grid2401"].legend()
 axd["Grid2401"].set(xlabel=f'{xLabel}', ylabel="R2")
 axd["Grid2401"].set_title("Grid2401", fontweight='bold')
-axd["Grid2401"].set_xlim([minMETRIC, minMETRIC])
+axd["Grid2401"].set_xlim([minMETRIC, maxMETRIC])
 axd["Grid2401"].set_ylim([minR2, maxR2])
 
 axd["Sobol1"].scatter(subsetSobol1[f'{metrictype}'], subsetSobol1["r2"], label="Sobol1", c="#DDCC77", marker='o')#, edgecolor="#44AA99")
 axd["Sobol1"].legend()
 axd["Sobol1"].set(xlabel=f'{xLabel}', ylabel="R2")
 axd["Sobol1"].set_title("Sobol1", fontweight='bold')
-axd["Sobol1"].set_xlim([minMETRIC, minMETRIC])
+axd["Sobol1"].set_xlim([minMETRIC, maxMETRIC])
 axd["Sobol1"].set_ylim([minR2, maxR2])
 
 axd["Sobol2"].scatter(subsetSobol2[f'{metrictype}'], subsetSobol2["r2"], label="Sobol2", c="#117733", marker='o')#, edgecolor="#44AA99")
 axd["Sobol2"].legend()
 axd["Sobol2"].set(xlabel=f'{xLabel}', ylabel="R2")
 axd["Sobol2"].set_title("Sobol2", fontweight='bold')
-axd["Sobol2"].set_xlim([minMETRIC, minMETRIC])
+axd["Sobol2"].set_xlim([minMETRIC, maxMETRIC])
 axd["Sobol2"].set_ylim([minR2, maxR2])
 
 plt.tight_layout()
