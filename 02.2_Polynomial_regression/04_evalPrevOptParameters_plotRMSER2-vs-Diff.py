82245f11e7cfd954ec45698c24032a3de8fa6d23
Robo <rstrickstrock@gmail.com>
2025-02-03 17:29:24 +0100

    polyRegr: added MAPE to 04_

--- 02.2_Polynomial_regression/04_evalPrevOptParameters_plotRMSER2-vs-Diff.py
+++ 02.2_Polynomial_regression/04_evalPrevOptParameters_plotRMSER2-vs-Diff.py
@@ -186,6 +186,7 @@ if createCombinedDataFile:
                                  "dataset":[],
                                  "degree":[],
                                  "rmse": [],
+                                 "mape": [],
                                  "r2": [],
                                  "prediction":[],
                                  "target":[],
@@ -264,7 +265,7 @@ if createCombinedDataFile:
       #print(f'{datasetSubset}')
       pass
     
-    ## get RMSE and R2 from dfStatistics entry  
+    ## get RMSE, MAPE and R2 from dfStatistics entry  
     try:
       thisRMSE = datasetSubset["rmse"]
     except:
@@ -279,6 +280,22 @@ if createCombinedDataFile:
         print(f'Could not cast \'thisRMSE = float(thisRMSE.to_numpy())\' for\nthisPredictionEntry = {thisPredictionEntry}\nthisStatisticsEntry = {datasetSubset}')
       else:
         #print(f'{thisRMSE}')
+        pass
+      pass
+    try:
+      thisMAPE = datasetSubset["mape"]
+    except:
+      print(f'Could not get \'thisMAPE = datasetSubset["mape"]\' for\nthisPredictionEntry = {thisPredictionEntry}\nthisStatisticsEntry = {datasetSubset}')
+    else:
+      #print(f'{thisMAPE}')
+      #print(f'{thisMAPE.to_numpy()}')
+      #print(f'{float(thisMAPE.to_numpy())}')
+      try:
+        thisMAPE = float(thisMAPE.to_numpy())
+      except:
+        print(f'Could not cast \'thisMAPE = float(thisMAPE.to_numpy())\' for\nthisPredictionEntry = {thisPredictionEntry}\nthisStatisticsEntry = {datasetSubset}')
+      else:
+        #print(f'{thisMAPE}')
         pass
       pass
     try:
@@ -303,6 +320,7 @@ if createCombinedDataFile:
                                             "dataset":[thisDataset],
                                             "degree":[thisDegree],
                                             "rmse": [thisRMSE],
+                                            "mape": [thisMAPE]
                                             "r2": [thisR2],
                                             "prediction":[thisPrediction],
                                             "target":[707.0],
@@ -335,7 +353,15 @@ idxMinRMSE = dfCombinedData['rmse'].idxmin()
 minRMSE = dfCombinedData['rmse'].min()
 #print(f'{minRMSE}')
 minRMSERow = dfCombinedData.iloc[idxMinRMSE]
-#print(f'Min RMSE Entry:\n{minRMSERow}\n')
+print(f'Min RMSE Entry:\n{minRMSERow}\n')
+
+## min MAPE
+idxMinMAPE = dfCombinedData['mape'].idxmin()
+#print(f'{idxMinMAPE}')
+minMAPE = dfCombinedData['mape'].min()
+#print(f'{minMAPE}')
+minMAPERow = dfCombinedData.iloc[idxMinMAPE]
+print(f'Min RMSE Entry:\n{minMAPERow}\n')
 
 ## max R2
 idxMaxR2 = dfCombinedData['r2'].idxmax()
@@ -343,7 +369,7 @@ idxMaxR2 = dfCombinedData['r2'].idxmax()
 maxR2 = dfCombinedData['r2'].max()
 #print(f'{maxR2}')
 maxR2Row = dfCombinedData.iloc[idxMaxR2]
-#print(f'Max R2 Entry:\n{maxR2Row}\n')
+print(f'Max R2 Entry:\n{maxR2Row}\n')
 
 ## diff closest to 0
 thisDiff_old = 100000000
@@ -362,9 +388,10 @@ subsetSobol1 = dfCombinedData[dfCombinedData["dataset"] == "Sobol1"]
 subsetSobol2 = dfCombinedData[dfCombinedData["dataset"] == "Sobol2"]
 
 ## plots
-gs_kw = dict(width_ratios=[1, 1, 1], height_ratios=[1])
-fig, axd = plt.subplot_mosaic([['RMSEvsR2', 'RMSEvsDiff', 'R2vsDiff']], 
-                               gridspec_kw=gs_kw, figsize=(21.0, 7.0))
+gs_kw = dict(width_ratios=[1, 1, 1], height_ratios=[1, 1])
+fig, axd = plt.subplot_mosaic([['RMSEvsR2', 'MAPEvsR2', 'RMSEvsMAPE'],
+                               ['RMSEvsDiff', 'MAPEvsDiff', 'R2vsDiff']], 
+                               gridspec_kw=gs_kw, figsize=(24.0, 16.0))
 
 axd["RMSEvsR2"].scatter(subsetGrid1296["rmse"], subsetGrid1296["r2"], label="Dataset: Grid1296", c="#332288")
 axd["RMSEvsR2"].scatter(subsetGrid2401["rmse"], subsetGrid2401["r2"], label="Dataset: Grid2401", c="#88CCEE")
@@ -373,6 +400,23 @@ axd["RMSEvsR2"].scatter(subsetSobol2["rmse"], subsetSobol2["r2"], label="Dataset
 axd["RMSEvsR2"].set(xlabel="RMSE", ylabel="R2")
 axd["RMSEvsR2"].set_title("RMSE vs R2", fontweight='bold')
 axd["RMSEvsR2"].legend()
+#
+axd["MAPEvsR2"].scatter(subsetGrid1296["mape"], subsetGrid1296["r2"], label="Dataset: Grid1296", c="#332288")
+axd["MAPEvsR2"].scatter(subsetGrid2401["mape"], subsetGrid2401["r2"], label="Dataset: Grid2401", c="#88CCEE")
+axd["MAPEvsR2"].scatter(subsetSobol1["mape"], subsetSobol1["r2"], label="Dataset: Sobol1", c="#DDCC77")
+axd["MAPEvsR2"].scatter(subsetSobol2["mape"], subsetSobol2["r2"], label="Dataset: Sobol2", c="#117733")
+axd["MAPEvsR2"].set(xlabel="MAPE", ylabel="R2")
+axd["MAPEvsR2"].set_title("MAPE vs R2", fontweight='bold')
+axd["MAPEvsR2"].legend()
+#
+axd["RMSEvsMAPE"].scatter(subsetGrid1296["rmse"], subsetGrid1296["mape"], label="Dataset: Grid1296", c="#332288")
+axd["RMSEvsMAPE"].scatter(subsetGrid2401["rmse"], subsetGrid2401["mape"], label="Dataset: Grid2401", c="#88CCEE")
+axd["RMSEvsMAPE"].scatter(subsetSobol1["rmse"], subsetSobol1["mape"], label="Dataset: Sobol1", c="#DDCC77")
+axd["RMSEvsMAPE"].scatter(subsetSobol2["rmse"], subsetSobol2["mape"], label="Dataset: Sobol2", c="#117733")
+axd["RMSEvsMAPE"].set(xlabel="RMSE", ylabel="MAPE")
+axd["RMSEvsMAPE"].set_title("RMSE vs MAPE", fontweight='bold')
+axd["RMSEvsMAPE"].legend()
+
 
 axd["RMSEvsDiff"].scatter(subsetGrid1296["rmse"], subsetGrid1296["diff"], label="Dataset: Grid1296", c="#332288")
 axd["RMSEvsDiff"].scatter(subsetGrid2401["rmse"], subsetGrid2401["diff"], label="Dataset: Grid2401", c="#88CCEE")
@@ -381,7 +425,15 @@ axd["RMSEvsDiff"].scatter(subsetSobol2["rmse"], subsetSobol2["diff"], label="Dat
 axd["RMSEvsDiff"].set(xlabel="RMSE", ylabel="Diff")
 axd["RMSEvsDiff"].set_title("RMSE vs Diff (target - pred(opt_params))", fontweight='bold')
 axd["RMSEvsDiff"].legend()
-
+#
+axd["MAPEvsDiff"].scatter(subsetGrid1296["mape"], subsetGrid1296["diff"], label="Dataset: Grid1296", c="#332288")
+axd["MAPEvsDiff"].scatter(subsetGrid2401["mape"], subsetGrid2401["diff"], label="Dataset: Grid2401", c="#88CCEE")
+axd["MAPEvsDiff"].scatter(subsetSobol1["mape"], subsetSobol1["diff"], label="Dataset: Sobol1", c="#DDCC77")
+axd["MAPEvsDiff"].scatter(subsetSobol2["mape"], subsetSobol2["diff"], label="Dataset: Sobol2", c="#117733")
+axd["MAPEvsDiff"].set(xlabel="MAPE", ylabel="Diff")
+axd["MAPEvsDiff"].set_title("MAPE vs Diff (target - pred(opt_params))", fontweight='bold')
+axd["MAPEvsDiff"].legend()
+#
 axd["R2vsDiff"].scatter(subsetGrid1296["r2"], subsetGrid1296["diff"], label="Dataset: Grid1296", c="#332288")
 axd["R2vsDiff"].scatter(subsetGrid2401["r2"], subsetGrid2401["diff"], label="Dataset: Grid2401", c="#88CCEE")
 axd["R2vsDiff"].scatter(subsetSobol1["r2"], subsetSobol1["diff"], label="Dataset: Sobol1", c="#DDCC77")
@@ -389,6 +441,7 @@ axd["R2vsDiff"].scatter(subsetSobol2["r2"], subsetSobol2["diff"], label="Dataset
 axd["R2vsDiff"].set(xlabel="R2", ylabel="Diff")
 axd["R2vsDiff"].set_title("R2 vs Diff (target - pred(opt_params))", fontweight='bold')
 axd["R2vsDiff"].legend()
+
 plt.tight_layout()
 plt.show()
 
