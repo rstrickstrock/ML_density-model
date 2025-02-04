cd3e67c942fc5341c731a772d39c46368e7623b2
rstric2s <robin.strickstrock@smail.emt.h-brs.de>
2025-02-04 16:54:05 +0100

    trained 02.3

--- 02.3_RandomForest_regression/01_randomForestRegression.py
+++ 02.3_RandomForest_regression/01_randomForestRegression.py
@@ -6,6 +6,7 @@ from sklearn.metrics import mean_absolute_percentage_error as skmape
 import pandas as pd
 import numpy as np
 import os
+import shutil
 #from scipy import stats
 from matplotlib import pyplot as plt
 
