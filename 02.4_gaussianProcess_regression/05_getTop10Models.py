eeaa910b1633c3cce98aea5509660833e50e5e92
rstric2s <robin.strickstrock@smail.emt.h-brs.de>
2025-02-06 15:32:21 +0100

    GPR filtered for best Models

--- 02.4_gaussianProcess_regression/05_getTop10Models.py
+++ 02.4_gaussianProcess_regression/05_getTop10Models.py
@@ -1,9 +1,11 @@
 import pandas as pd
-
 import os
+import shutil
 
 
 statisticsFile = 'Stats.csv'
+modelsDir = 'trainedModels'  
+bestModelsDir = 'bestTrainedModels'
 
 if not os.path.isfile(statisticsFile):
   print(f'Can not find and open \'{statisticsFile}\'. Exit.')
@@ -18,11 +20,24 @@ else:
   else:
     #print(f'{dfStatistics}')
     pass
-  
-  
+
+
+pwd = os.getcwd()
+cwd = os.path.join(pwd, bestModelsDir)
+#print(f'{cwd}')
+if os.path.exists(cwd):
+  shutil.rmtree(cwd)
+os.mkdir(cwd)
+
+
 ## min MAPE
 dfThisStatistics = dfStatistics
 minMapeIDXs = []
+thisBestModelsDir = 'minMAPE'
+
+thisCWD = os.path.join(cwd, thisBestModelsDir)
+os.mkdir(thisCWD)
+
 for i in range(0, 10):
   idxMinMAPE = dfThisStatistics['mape'].idxmin()
   #print(f'{idxMinMAPE}')
@@ -32,12 +47,21 @@ for i in range(0, 10):
   #print(f'Min MAPE Entry:\n{minMAPERow}\n')
   minMapeIDXs.append(idxMinMAPE)
   dfThisStatistics = dfThisStatistics.drop([idxMinMAPE])
+  srcModelPath = os.path.join(modelsDir, f'{dfStatistics.iloc[idxMinMAPE]["ratio"]}', f'trained_model{dfStatistics.iloc[idxMinMAPE]["dataset"]}_{dfStatistics.iloc[idxMinMAPE]["ratio"]}_{int(dfStatistics.iloc[idxMinMAPE]["rndint"])}_{dfStatistics.iloc[idxMinMAPE]["kernel"]}.sav')
+  print(f'{srcModelPath}')
+  dstModelPath = os.path.join(thisCWD, f'trained_model{dfStatistics.iloc[idxMinMAPE]["dataset"]}_{dfStatistics.iloc[idxMinMAPE]["ratio"]}_{int(dfStatistics.iloc[idxMinMAPE]["rndint"])}_{dfStatistics.iloc[idxMinMAPE]["kernel"]}.sav')
+  shutil.copy(srcModelPath, dstModelPath)
 
-print(f'{minMapeIDXs}')
+#print(f'{minMapeIDXs}')
 
 ## max R2
 dfThisStatistics = dfStatistics
 maxR2IDXs = []
+thisBestModelsDir = 'maxR2'
+
+thisCWD = os.path.join(cwd, thisBestModelsDir)
+os.mkdir(thisCWD)
+
 for i in range(0, 10):
   idxMaxR2 = dfThisStatistics['r2'].idxmax()
   #print(f'{idxMaxR2}')
@@ -47,6 +71,10 @@ for i in range(0, 10):
   #print(f'Max R2 Entry:\n{maxR2Row}\n')
   maxR2IDXs.append(idxMaxR2)
   dfThisStatistics = dfThisStatistics.drop([idxMaxR2])
+  srcModelPath = os.path.join(modelsDir, f'{dfStatistics.iloc[idxMaxR2]["ratio"]}', f'trained_model{dfStatistics.iloc[idxMaxR2]["dataset"]}_{dfStatistics.iloc[idxMaxR2]["ratio"]}_{int(dfStatistics.iloc[idxMaxR2]["rndint"])}_{dfStatistics.iloc[idxMaxR2]["kernel"]}.sav')
+  print(f'{srcModelPath}')
+  dstModelPath = os.path.join(thisCWD, f'trained_model{dfStatistics.iloc[idxMaxR2]["dataset"]}_{dfStatistics.iloc[idxMaxR2]["ratio"]}_{int(dfStatistics.iloc[idxMaxR2]["rndint"])}_{dfStatistics.iloc[idxMaxR2]["kernel"]}.sav')
+  shutil.copy(srcModelPath, dstModelPath)
 
 #print(f'{maxR2IDXs}')
 print(f'Min MAPE Models:')
